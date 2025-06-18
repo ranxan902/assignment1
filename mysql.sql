010f0eb1a8ab09d4cffd321fbcced332b7294e48
EC2 Default User <ec2-user@ip-172-31-66-154.ec2.internal>
2025-06-18 01:00:17 +0000

    nano added

--- mysql.sql
+++ mysql.sql
@@ -10,6 +10,7 @@ location VARCHAR(20));
 
 INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
 INSERT INTO employee VALUES ('2','Amanda','Williams','Smile','local');
-INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('3','Alan','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('4','Alan','Williams','Empathy','alien');
 SELECT * FROM employee;
 
